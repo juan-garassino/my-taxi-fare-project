dd140dbc2011aa3c16b8cfc21a60b14cc3c0743f
Johninni <juan.garassino@hotmail.com>
2020-11-16 21:24:05 +0100

    Trying to fix the problem

--- mlproject/try.py
+++ mlproject/try.py
@@ -1,3 +1,8 @@
 def try_me():
     # imprime algo
     return "Hola Griseldo... espero que este todo muy bien"
+
+if __name__ == '__main__':
+    # For introspections purpose to quickly get this functions on ipython
+    import mlproject
+    tryme()
